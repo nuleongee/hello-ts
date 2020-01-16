097c978ae90766d649423eab78276c99a939d15d
임수빈 <emsbn@ctsoft.kr>
2020-01-16 20:32:19 +0900

    1-5 중

--- lecture.js
+++ lecture.js
@@ -1,4 +1,14 @@
 "use strict";
-const a = 1;
-console.log("hello javascript");
-console.log("hello typescript");
+let numberOne = Math.ceil(Math.random() * 9);
+let numberTwo = Math.ceil(Math.random() * 9);
+let result = nuberOne * numberTwo;
+let string = 'hello';
+let boolean = true;
+const word = document.createElement('div');
+word.textContent = `${numberOne} 곱하기 ${numberTwo}?`;
+document.body.append(word);
+const form = document.createElement('form');
+document.body.append(form);
+const input = document.createElement('input');
+input.type = 'number';
+input.toFixed(3);
