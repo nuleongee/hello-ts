d89861c15f1455d1b7b8a1e95afcb2275cd3020a
emsbn <emsbn1@gmail.com>
2020-02-06 23:36:08 +0900

    custom d.ts

---
+++ lecture/lecture.ts
@@ -0,0 +1,7 @@
+import canUseDom from "can-use-dom";
+
+console.log(canUseDom);
+
+window.hello = "a";
+const error = new Error("");
+error.code;
---
+++ lecture/types/index.d.ts
@@ -0,0 +1,10 @@
+export {};
+declare global {
+  interface Window {
+    hello: string;
+  }
+
+  interface Error {
+    code?: any;
+  }
+}
