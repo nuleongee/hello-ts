ccd7edfe94a4fb1fc1cef8f394db4a94325dac4d
emsbn <emsbn1@gmail.com>
2020-02-06 23:59:24 +0900

    declaration, declarationDir

---
+++ lecture/lecture.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var can_use_dom_1 = require("can-use-dom");
+console.log(can_use_dom_1["default"]);
+window.hello = "a";
+var error = new Error("");
+error.code;
