060d5a02ac0936ee887bba489c95b02190d8edcd
임수빈 <emsbn@ctsoft.kr>
2020-02-07 11:01:14 +0900

    intersection, call 제네릭

--- lecture/lecture.ts
+++ lecture/lecture.ts
@@ -5,3 +5,11 @@ console.log(canUseDom);
 window.hello = "a";
 const error = new Error("");
 error.code;
+
+const result = Array.prototype.map.call<
+  number[],
+  [(item: number) => string],
+  string[]
+>([1, 2, 3], item => {
+  return item.toFixed(1);
+});
